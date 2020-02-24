dc5bb7b1b2d78e223f8f0b4f404f4d1f4c7f9d9b
Vladimir Novick <vladimirnovick@Vladimirs-MacBook-Pro.local>
2020-02-24 15:36:46 +0200

    add auth current session

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,17 +2,25 @@ import React from "react"
 import { Link } from "gatsby"
 
 import { withAuthenticator } from 'aws-amplify-react'
+import { Auth } from 'aws-amplify';
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
+const SecondPage = () => {
+
+  Auth.currentSession()
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
+  return (
+    <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to protected page</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
-)
+  )
+}
 
 export default withAuthenticator(SecondPage)
